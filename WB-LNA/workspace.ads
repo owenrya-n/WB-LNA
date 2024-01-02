f32bac6e9324296a4f79e51bab8a36b6a6f80eaa
owenrya-n <owenryan1883@gmail.com>
2024-01-01 22:19:30 -0800

    Basic Architecture in ADS

---
+++ WB-LNA/workspace.ads
@@ -0,0 +1,31 @@
+<ADSWorkspace Revision="1" Version="100">
+    <Workspace Name="">
+        <LibraryDefs Name="lib.defs" />
+        <ConfigFile Name="de_sim.cfg" />
+        <ConfigFile Name="hpeesofsim.cfg" />
+        <Log Name="search_history.log" />
+        <Preferences Name="layout.prf" />
+        <Preferences Name="schematic.prf" />
+        <Preferences Name="WB-LNA_lib_lay.prf" />
+        <Library Name="ads_standard_layers" />
+        <Library Name="ads_schematic_layers" />
+        <Library Name="empro_standard_layers" />
+        <Library Name="ads_builtin" />
+        <Library Name="ads_standard_layers_ic" />
+        <Library Name="ads_schematic_layers_ic" />
+        <Library Name="ads_schematic_ports_ic" />
+        <Library Name="ads_rflib" />
+        <Library Name="ads_sources" />
+        <Library Name="ads_simulation" />
+        <Library Name="ads_tlines" />
+        <Library Name="ads_bondwires" />
+        <Library Name="ads_datacmps" />
+        <Library Name="ads_behavioral" />
+        <Library Name="ads_textfonts" />
+        <Library Name="ads_common_cmps" />
+        <Library Name="ads_designs" />
+        <Library Name="ads_pelib" />
+        <Library Name="WB-LNA_lib" />
+        <Cell Name="WB-LNA_lib:Top" />
+    </Workspace>
+</ADSWorkspace>
